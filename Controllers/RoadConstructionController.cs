fb2954290b12966a5abe923e46ba5efc6ecdbbb9
Sanjana11032 <sanjana.cybrain@gmail.com>
2025-04-25 18:39:34 +0530

    Road Construction Apis

---
+++ Controllers/RoadConstructionController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using roadconstruction_apis.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+
+
+namespace roadconstruction_apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoadConstructionController : ControllerBase
+    {
+        private readonly string filePath = "Data/data.json";
+
+        [HttpGet("Get")]
+        public IActionResult Get()
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                var json = System.IO.File.ReadAllText(filePath);
+                var data = JsonSerializer.Deserialize<RoadConstruction>(json);
+
+                return data != null ? Ok(data) : NotFound("Data not found.");
+            }
+
+            return NotFound("File not found.");
+        }
+
+
+        [HttpPost("update")]
+        public IActionResult update([FromBody] Data input)
+        {
+            if (!System.IO.File.Exists(filePath))
+                return NotFound("JSON file not found.");
+
+            var json = System.IO.File.ReadAllText(filePath);
+            var existingData = JsonSerializer.Deserialize<RoadConstruction>(json);
+
+            var match = existingData.Datas.FirstOrDefault(d => d.SamplingTime == input.SamplingTime);
+
+            if (match != null)
+            {
+                match.Properties = input.Properties;
+            }
+            else
+            {
+                existingData.Datas.Add(input);
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+
+            string updatedJson = JsonSerializer.Serialize(existingData, options);
+            System.IO.File.WriteAllText(filePath, updatedJson);
+
+            return Ok(new { message = "Data Updated successfully." });
+        }
+    
+    }
+}
---
+++ Models/RoadConstruction.cs
@@ -0,0 +1,21 @@
+﻿namespace roadconstruction_apis.Models
+{
+    public class RoadConstruction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Data> Datas { get; set; }
+    }
+
+    public class Data
+    {
+        public string SamplingTime { get; set; }
+        public List<Property> Properties { get; set; }
+    }
+
+    public class Property
+    {
+        public string Label { get; set; }
+        public object Value { get; set; }
+    }
+}
